023d6da21e2d8ea1d4a97cc05ebde495ebb162c9
williamruiz <william.ruiz@origen.bio>
2023-08-11 22:59:29 +0200

    Add my first controller

--- MyCV.API/Program.cs
+++ MyCV.API/Program.cs
@@ -18,6 +18,10 @@ if (app.Environment.IsDevelopment())
 	app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyCV.Api v1"));
 	//For now I will work witoout DataBase, uncomment this line to create DataBase
 	//app.ApplyMigrations(); 
+	//Commands to run migrations:
+	//dotnet ef migrations add InitialCreate --project MyCV.Infrastructure --startup-project MyCV.API -o Persistence/Migrations
+	 //In order to update the database, run the following command:
+	 //dotnet ef database update -p MyCV.Infrastructure -s MyCV.API
 }
 
 app.UseHttpsRedirection();
