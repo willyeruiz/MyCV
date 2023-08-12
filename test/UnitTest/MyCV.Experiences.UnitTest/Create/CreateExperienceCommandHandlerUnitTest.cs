9807b18c4883cd3a0f51a018a33f47a412d15bde
williamruiz <william.ruiz@origen.bio>
2023-08-12 11:08:45 +0200

    test project

---
+++ test/UnitTest/MyCV.Experiences.UnitTest/Create/CreateExperienceCommandHandlerUnitTest.cs
@@ -0,0 +1,15 @@
+namespace MyCV.Experiences.UnitTest.Create;
+
+public class CreateExperienceCommandHandlerUnitTest
+{
+    
+    //Que voy a probar
+    //Escenario
+    //Lo que deberia pasar
+
+    [Fact]
+    public void CreateExperienceCommandHandlerUnitTest()
+    {
+        
+    }
+} 
